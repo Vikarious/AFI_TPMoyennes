debe2e034a480ae83193d9fbcdb4e5bb504c4c2d
Vikarious <fialaire.arnaud@free.fr>
2023-05-15 11:19:02 +0200

    upload github

---
+++ Classe.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    public class Classe
+    {
+        public string nomClasse 
+        { 
+            get; 
+            private set;
+        }
+        public List<string> matieres 
+        { 
+            get; 
+            private set; 
+        }
+        public List<Eleve> eleves 
+        { get; 
+            private set; 
+        }
+
+        public Classe(string nomClasse)
+        {
+            this.nomClasse = nomClasse;
+            this.matieres = new List<string>();
+            this.eleves = new List<Eleve>();
+        }
+
+        public void ajouterEleve(string prenom, string nom)
+        {
+            this.eleves.Add(new Eleve(prenom, nom));
+        }
+
+        public void ajouterMatiere(string matiere)
+        {
+            this.matieres.Add(matiere);
+        }
+
+        public float Moyenne(int matiere)
+        {
+            var moyennesEleves = this.eleves.Select(e => e.Moyenne(matiere)).ToList();
+            if (!moyennesEleves.Any()) return 0;
+            return (float)Math.Truncate(100 * moyennesEleves.Average()) / 100;
+        }
+
+        public float Moyenne()
+        {
+            var moyennesMatieres = new List<float>();
+            for (int i = 0; i < this.matieres.Count; i++)
+            {
+                moyennesMatieres.Add(Moyenne(i));
+            }
+            return (float)Math.Truncate(100 * moyennesMatieres.Average()) / 100;
+        }
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    public class Eleve
+    {
+        public string prenom 
+        { 
+            get; 
+            private set;
+        }
+        public string nom 
+        { 
+            get; 
+            private set;
+        }
+        public List<Note> notes 
+        { 
+            get; 
+            private set; 
+        }
+
+        public Eleve(string prenom, string nom)
+        {
+            this.prenom = prenom;
+            this.nom = nom;
+            this.notes = new List<Note>();
+        }
+
+        public void ajouterNote(Note note)
+        {
+            this.notes.Add(note);
+        }
+
+        public float Moyenne(int matiere)
+        {
+            var notesMatiere = this.notes.Where(n => n.matiere == matiere).ToList();
+            if (!notesMatiere.Any()) return 0;
+            return (float)Math.Truncate(100 * notesMatiere.Average(n => n.note)) / 100;
+        }
+
+        public float Moyenne()
+        {
+            if (!this.notes.Any()) return 0;
+            var moyennes = new List<float>();
+            var matieres = this.notes.Select(n => n.matiere).Distinct();
+            foreach (var matiere in matieres)
+            {
+                moyennes.Add(Moyenne(matiere));
+            }
+            return (float)Math.Truncate(100 * moyennes.Average()) / 100;
+        }
+    }
+}
